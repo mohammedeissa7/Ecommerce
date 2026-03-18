23113b782695f3732d4a0beca0eb54c6aead9a89
Eissa <smme012345@gmail.com>
2026-03-18 03:51:05 +0200

    feat[backend]: connect to mongo DB

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connect } from 'mongoose';
+import connectDB from './config/DB';
 
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+await connectDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
