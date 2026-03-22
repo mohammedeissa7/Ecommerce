e3804062196201e67df8a909b07af0304732a357
Eissa <smme012345@gmail.com>
2026-03-22 18:58:23 +0200

    feat[backend]: add hash password function to user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
-import { mongoose } from 'mongoose';
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,6 +40,21 @@ const userSchema = new mongoose.Schema({
         timestamps: true,
     }
 );
+
+userSchema.pre('save', async (next) => {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+userSchema.methods.comparePassword = async (password) => {
+    return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 export default User;
