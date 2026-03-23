70479c04062b1c79bc254233b9b78987ff148140
Eissa <smme012345@gmail.com>
2026-03-23 03:49:26 +0200

    feat[backend]: add signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,21 @@
 import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
-    console.log("Sign-up");
+    const { name, email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already in use" });
+        }
+
+        const newUser = await User.create({ name, email, password });
+        await newUser.save();
+
+        res.status(201).json({ user: newUser, message: "User created successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 export const signin = async (req, res) => {
