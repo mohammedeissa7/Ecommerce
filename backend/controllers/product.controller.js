0c53f1ab34b06591024988d23af765bc996901d3
Eissa <smme012345@gmail.com>
2026-03-24 03:55:35 +0200

    feat[backend]:  add create Product function

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -31,3 +31,28 @@ export const getFeaturedProducts = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+export const createProduct = async (req, res) => {
+    try{
+        const { name, description, price, image , category } = req.body;
+        let cloudinaryResponse = null;
+        if (image) {
+            cloudinaryResponse = await cloudinary.uploader.upload(image, {
+                folder: 'products',
+            });
+        }
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
+            category,
+        });
+        
+        res.status(201).json(product);
+
+    }
+        catch (error) {
+        res.status(500).json({ message: error.message });
+        }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,6 +1,6 @@
 import express from 'express';      
 import { adminRoute, protectRoute } from '../middleware/auth.middleware.js';
-import { getAllProducts, getFeaturedProducts } from '../controllers/product.controller.js';
+import { createProduct, getAllProducts, getFeaturedProducts } from '../controllers/product.controller.js';
 
 
 const productRouter = express.Router();
@@ -8,5 +8,6 @@ const productRouter = express.Router();
 
 productRouter.get('/', protectRoute,adminRoute, getAllProducts);
 productRouter.get('/featured', getFeaturedProducts);
+productRouter.post('/', protectRoute, adminRoute, createProduct);
 
 export default productRouter;
