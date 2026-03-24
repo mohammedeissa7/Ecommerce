479c8388d538e3cd80675b40356d1efe14d82191
Eissa <smme012345@gmail.com>
2026-03-24 04:51:47 +0200

    feat[backend]: add  the add product to cart  function to cart controller

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -12,5 +12,23 @@ export const getCartProducts = async (req, res) => {
         console.error(error);
         res.status(500).json({ message: "Internal server error" });
     }
-};  
+};
 
+
+export const addToCart = async (req, res) => {
+    try {
+        const { productId } = req.body;
+        const user = req.user;
+        const existingItem = user.cartItems.find((item) => item.id === productId);
+        if (existingItem) {
+            existingItem.quantity += 1;
+        } else {
+            user.cartItems.push(productId);
+        }
+        await user.save();
+        res.json(user.cartItems);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
