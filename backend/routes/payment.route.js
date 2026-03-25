95ee742d63645a36f4a3f3b61ee66dba02722e61
Eissa <smme012345@gmail.com>
2026-03-25 04:05:13 +0200

    feat[backend]: add create checkout session function to payment controller

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { createCheckoutSession } from '../controllers/payment.controller.js';
 
 const paymentRouter = express.Router();
 
-
+paymentRouter.post("/create-checkout-session", protectRoute, createCheckoutSession);
 
 export default paymentRouter;
